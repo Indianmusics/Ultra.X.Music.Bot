e26d9f4de180c0d4f220fb2f7cb6a525df23d0ef
Indianmusics <113574739+Indianmusics@users.noreply.github.com>
2022-10-24 12:28:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,37 +8,37 @@ admins = {}
 
 #:::::::::::::@OFFICIALHACKERERA
 
-API_ID = int(getenv("API_ID", ""))
+API_ID = int(getenv("API_ID", "13152319"))
 
-API_HASH = getenv("API_HASH", "")
+API_HASH = getenv("API_HASH", "ba87df5eaee5018229c8f7c04a51e03c")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "5371685561:AAFl7iaV5Vw7rhoYc7i41_VbouoMxp7SMmU")
 
-DEEPAK_SESSION = getenv("DEEPAK_SESSION", "")
+DEEPAK_SESSION = getenv("DEEPAK_SESSION", "BQAeIrRyabKukgs1-2vqvhtrfHdyW7i4Qy7o-n6eG-GfGAiJtOTSlGxlD5F-CwYuapLyOdwj9l-VuPebnl6ZXfRf2D2B9ot0A-4wKX1IvVLxIRSnyWkqsO-Du_NpecWWp_J8vM2x5Y60yO7tZZrEZuunp65s5ZcwvVbWVZB90XpzR5uuFpYli5JY-c5hq3dzpu07iP876H-Bn3bvQXufBmzI0fZe1_7GpM4ARkU4-RDjrDd7KU5Ez0ATxBf1InQmixKU2k9qX0zo5vx_g1i99k77N_INeN7WQqUwKMxpSaS45w1dpA2Wk9w6MX-1krrMyMxo0mj1zHFnoWLDOjSRz1SFAAAAATMjpU0A")
 
-OWNER_ID = getenv("OWNER_ID", "2035388821")
+OWNER_ID = getenv("OWNER_ID", "5152941389")
 
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001880824438"))
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5152941389").split()))
 
 MONGO_URL = os.environ.get("MONGO_URL", "")
 
-OWNER_NAME = getenv("OWNER_NAME", "OFFICIALHACKERERA")
+OWNER_NAME = getenv("OWNER_NAME", "Rajanmusicxd")
 
-BOT_NAME = getenv("BOT_NAME", "Ultra X Music")
+BOT_NAME = getenv("BOT_NAME", "Rajan")
 
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "【𝄞≛⃝.݆݆݆݆ۗۗۗۗۗۗۗۗۗۗۗۗۗۗۗۗۗۗۗۗۗۗۗۗۗۗۗۗۗۗۗۗۗۗۗۗ🇷𝐀𝐉𝐀𝐍 ❤️ ≛⃝🇵𝗔𝗡𝗗𝗜𝗧")
 
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "rajanmusic285")
 
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "rajanmusic2852")
 
-OWNER_USERNAME = getenv("OWNER_USERNAME", "")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "RajanPanditxd")
 
-ALIVE_NAME = getenv("ALIVE_NAME", "")
+ALIVE_NAME = getenv("ALIVE_NAME", "Rajan")
 
-BOT_USERNAME = getenv("BOT_USERNAME", "")
+BOT_USERNAME = getenv("BOT_USERNAME", "Rajan_MusicBot")
 
 
 
